c5180ba37c6cc27028a88492badf116c45a30994
Timur Latypov <timur.latypov@gmail.com>
2019-11-04 16:13:52 +0300

    Develop (#3)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,35 +1,41 @@
 module.exports = {
     apps: [
         {
-            name: "client",
+            name: "chito-ra",
+            append_env_to_name: true,
             script: "npm",
             args: "start",
             watch : true,
-            env: {
+            env_develop: {
+                NODE_ENV: "develop",
+                HOST: "127.0.0.1",
+                PORT: 3001,
+            },
+            env_production: {
+                NODE_ENV: "production",
                 HOST: "127.0.0.1",
                 PORT: 3000,
-                NODE_ENV: "production"
             }
         }
     ],
     deploy: {
-        prod: {
+        production: {
             user: "root",
             host: "193.124.178.241",
             key: "~/.ssh/id_rsa_chitora",
             ref: "origin/master",
             repo: "https://github.com/timurlatypov/chito-ra-client.git",
             path: "/var/www/chito-ra/client/production/",
-            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save"
+            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save --env production"
         },
-        dev : {
+        develop : {
             user: "root",
-            host: "194.58.120.209",
+            host: "193.124.178.241",
             key: "~/.ssh/id_rsa_chitora",
             ref: "origin/develop",
             repo: "https://github.com/timurlatypov/chito-ra-client.git",
-            path: "/var/www/client/dev/",
-            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save"
+            path: "/var/www/chito-ra/client/develop/",
+            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save --env develop"
         }
     }
 };
