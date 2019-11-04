5415b14fddd73ffbd5ee3a8ac79b85a508883aa9
Timur Latypov <timur.latypov@skillbox.ru>
2019-11-04 18:09:40 +0300

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,8 +11,8 @@ module.exports = {
                 NODE_ENV: "development"
             },
             env_production: {
-                HOST: "127.0.0.2",
-                PORT: 3000,
+                HOST: "127.0.0.1",
+                PORT: 4000,
                 NODE_ENV: "production"
             },
         }
