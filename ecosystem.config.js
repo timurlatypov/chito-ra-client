081ef3865a2f8dd034d5c0bb331a9e78123d1beb
Timur Latypov <timur.latypov@skillbox.ru>
2019-11-04 15:59:31 +0300

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,11 +24,11 @@ module.exports = {
         },
         dev : {
             user: "root",
-            host: "194.58.120.209",
+            host: "193.124.178.241",
             key: "~/.ssh/id_rsa_chitora",
             ref: "origin/develop",
             repo: "https://github.com/timurlatypov/chito-ra-client.git",
-            path: "/var/www/client/dev/",
+            path: "/var/www/chito-ra/client/develop/",
             "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save"
         }
     }
