1a3ff23fc858b3ea53691a03ce88857df2a3d6aa
Timur Latypov <timur.latypov@skillbox.ru>
2019-11-04 17:13:32 +0300

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,16 +6,25 @@ module.exports = {
             args: "start",
             watch : true,
             env: {
-                NODE_ENV: 'development',
-                PORT: 3001
+                "PORT": 4000,
+                "NODE_ENV": "development"
             },
             env_production: {
-                NODE_ENV: 'production',
-                PORT: 3000
+                "PORT": 3000,
+                "NODE_ENV": "production",
             }
         }
     ],
     deploy: {
+        development : {
+            user: "root",
+            host: "193.124.178.241",
+            key: "~/.ssh/id_rsa_chitora",
+            ref: "origin/develop",
+            repo: "https://github.com/timurlatypov/chito-ra-client.git",
+            path: "/var/www/chito-ra/client/develop/",
+            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save --env development"
+        },
         production: {
             user: "root",
             host: "193.124.178.241",
@@ -25,14 +34,5 @@ module.exports = {
             path: "/var/www/chito-ra/client/production/",
             "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save --env production"
         },
-        develop : {
-            user: "root",
-            host: "193.124.178.241",
-            key: "~/.ssh/id_rsa_chitora",
-            ref: "origin/develop",
-            repo: "https://github.com/timurlatypov/chito-ra-client.git",
-            path: "/var/www/chito-ra/client/develop/",
-            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js && pm2 save --env development"
-        }
     }
 };
